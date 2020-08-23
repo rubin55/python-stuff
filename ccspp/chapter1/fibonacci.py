3c5288a682bcd333ca95256602716a653d3f2349
Rubin Simons <rubin@raaf.tech>
2020-08-23 22:52:31 +0200

    Cached fibonacci functions.

--- ccspp/chapter1/fibonacci.py
+++ ccspp/chapter1/fibonacci.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python3
 
+from functools import lru_cache
 from sys import argv
+from typing import Dict
+
+memo: Dict[int, int] = {0: 0, 1: 1}
 
 
 def fib1(n: int) -> int:
-    print('fib1({0})'.format(n))
     return fib1(n - 1) + fib1(n - 2)
 
 
 def fib2(n: int) -> int:
-    print('fib2({0})'.format(n))
     if n < 2:
         return n
     return fib2(n - 1) + fib2(n - 2)
 
 
+def fib3(n: int) -> int:
+    if n not in memo:
+        memo[n] = fib3(n - 1) + fib3(n - 2)
+    return memo[n]
+
+
+@lru_cache(maxsize=None)
+def fib4(n: int) -> int:
+    if n < 2:
+        return n
+    return fib4(n - 1) + fib4(n - 2)
+
+
 if __name__ == '__main__':
-    print(fib2(int(argv[1])))
+    print(fib4(int(argv[1])))
